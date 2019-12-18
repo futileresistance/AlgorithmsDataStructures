022f268aa37852132fc4dfac0f057a9433b9dfdc
pz_neurohive <pavel.zubitskij@neurohive.io>
2019-12-18 12:30:17 +0300

    fx

--- SortingAlgorithms/quick_sort.py
+++ SortingAlgorithms/quick_sort.py
@@ -2,6 +2,6 @@ def quick_sort(arr):
     if len(arr) < 2:
         return arr
     pivot = arr[0]
-    less = [elem for elem in arr if elem < pivot]
-    more = [elem for elem in arr if elem > pivot]
+    less = [elem for elem in arr[1:] if elem < pivot]
+    more = [elem for elem in arr[1:] if elem >= pivot]
     return quick_sort(less) + [pivot] + quick_sort(more)
